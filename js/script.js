da626e40d9d05f679ba79f9e4a1b3bad888f247c
I.Smal <serbov@pochta.ru>
2017-04-07 11:43:39 +0300

    table side navigation

--- js/script.js
+++ js/script.js
@@ -1,42 +1,19 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-	//slider
+	//aside toggling
 	(function() {
 
 		//cache Dom
-		var $window = $(window);
-		var $slider = $('.js-slider');
+		var aside = document.querySelector('.js-aside');
+		var asideHamburger = document.querySelector('.js-aside-hamburger');
 
-		runSlick();
+		//bind events
+		asideHamburger.addEventListener('click', toggleAside);
 
-		//getOptions
-		function getOptions($slider) {
-			var slide = '#' + $slider.getAttribute('id') + ' .js-slider-item';
-			var slidesToShow = $slider.getAttribute('data-slides');
-			var slidesToShowSm = $slider.getAttribute('data-slides-sm');
-			var dots = $slider.getAttribute('data-dots');
-
-			return {
-				slide: slide,
-				slidesToShow: slidesToShow ? slidesToShow : '1',
-				dots: dots ? dots : true,
-				arrows: false,
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: slidesToShowSm ? slidesToShowSm : '1'
-						}
-					}
-				]
-			};
+		//toggleAside
+		function toggleAside(e) {
+			e.preventDefault();
+			aside.classList.toggle('_opened');
 		}
-
-		//run slick
-		function runSlick() {
-			for(var i = 0; i < $slider.length; i++) {
-				$($slider[i]).slick(getOptions($slider[i]));
-			}
-		}
-	})();
+	})()
 });
