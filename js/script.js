ef9b0d5e0e04a3b692adb4430f0598150c2e2841
I.Smal <serbov@pochta.ru>
2017-04-07 12:01:01 +0300

    fixed side nav

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 	(function() {
 
 		//cache Dom
+		var layout = document.querySelector('.js-layout');
 		var aside = document.querySelector('.js-aside');
 		var asideHamburger = document.querySelector('.js-aside-hamburger');
 
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
 		//toggleAside
 		function toggleAside(e) {
 			e.preventDefault();
+			layout.classList.toggle('_nav-opened');
 			aside.classList.toggle('_opened');
 		}
 	})()
